e4a7c4c11e98896c0d6278d221f50d1070af6e08
AureliaYen <ysnaj51348@gmail.com>
2023-05-29 00:05:02 +0800

    會員登入頁面v1

--- Controllers/SuperController.cs
+++ Controllers/SuperController.cs
@@ -1,9 +1,27 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Library.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
 
 namespace Library.Controllers
 {
     public class SuperController : Controller
     {
+        //設置登入限制，若須借書須先登入帳號
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            base.OnActionExecuting(context);
+            if (!HttpContext.Session.Keys.Contains(CDictionary.SK_LOGINED_USER))
+            {
+                context.Result = new RedirectToRouteResult(new RouteValueDictionary(new
+                {
+                    Controller = "Home",
+                    Action = "Login",
+                }));
+
+            }
+        }
+
+
         public IActionResult Index()
         {
             return View();
